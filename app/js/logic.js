f02e419434dcee6c5114757767b2c65baf37b260
Richard Zurad <rzurad@gmail.com>
2014-10-12 16:10:58 -0700

    It's gotta be a signal from Earth

--- app/js/logic.js
+++ app/js/logic.js
@@ -3,12 +3,32 @@ import ProcessManager from 'processing/process-manager';
 var WizardLogic;
 
 WizardLogic = Ember.Object.extend({
+    elapsedTime: 0,
+
+    state: function () {
+        return this.get('stateManager.currentState.name');
+    }.property('stateManager.currentState.name'),
+
     init: function () {
         this.set('views', []);
         this.set('processManager', ProcessManager.create());
 
         /* init random number generator */
-        /* set to initializing state */
+
+        // Create the [state machine](https://github.com/emberjs/ember-states/blob/master/packages/ember-states/lib/state_manager.js#L238) for the game logic
+        this.set('stateManager', Ember.StateManager.create({
+            // Set the logic object to the "initializing" state
+            initialState: 'initializing',
+
+            initializing: Ember.State.create(),
+            mainMenu: Ember.State.create(),
+            loadingGameEnvironment: Ember.State.create(),
+            waitingForPlayersToLoadEnvironments: Ember.State.create(),
+            spawnPlayersActors: Ember.State.create(),
+            waitingForPlayers: Ember.State.create(),
+            running: Ember.State.create()
+        }));
+
         /* create the level manager */
         /* initialize the level manager */
         /* RegisterEngineScriptEvents */
@@ -27,18 +47,59 @@ WizardLogic = Ember.Object.extend({
     },
 
     onUpdate: function (elapsedTime, deltaTime) {
-        /* update game elapsedTime */
-        /* check the wizardlogic state */
-            /* if Initializing, change to MainMenu state */
-            /* if MainMenu or LoadingGameEnvironment, do nothing */
+        var stateManager = this.get('stateManager');
+
+        // Update the `elapsedTime`.
+        this.set('elapsedTime', elapsedTime);
+        
+        // Every frame, we want to make sure that if we are in a place
+        // that requires a state change, we go ahead and make that state change.
+        switch (this.get('state')) {
+            case 'initializing':
+                // Since initialization is synchronous, we should never actually get to the
+                // `onUpdate` call every frame if the game logic was not done initializing. This
+                // means we want to transition to the default state: the `"mainMenu"` state
+                stateManager.transitionTo('mainMenu');
+                break;
+
+            // If we are in a frame callback and we are in either the `"mainMenu"` state or
+            // the `"loadingGameEnvironment"` state, don't do anything, because transitions
+            // out of those states depend on logic done by other functions.
+            case 'mainMenu':
+            case 'loadingGameEnvironment':
+                break;
+
             /* if WaitingForPlayersToLoadEnvironments, do nothing unless
              *      all players have loaded, in which case SpawnPlayersActors */
-            /* if SpawnPlayersActors state, switch to Running */
+            case 'waitingForPlayersToLoadEnvironments':
+                Ember.Logger.error('"watingForPlayersToLoadEnvironments" state not handled!');
+                break;
+
+            // Similarly, processing that needs to happen when in the `"spawnPlayersActors"`
+            // state should be happening within one Frame, so if we're in the `"spawnPlayersActors"`
+            // state, assume we don't need to be in it anymore and switch to the `"running"` state.
+            case 'spawnPlayersActors':
+                stateManager.transitionTo('running');
+                break;
+
             /* if WaitingForPlayers state, check if all players are attached and that
              *      we have a level to load from the server, if so, switch to LoadingGameEnvironment */
-            /* if Running, */
+            case 'waitingForPlayers':
+                Ember.Logger.error('"waitingForPlayers" state not handled!');
+                break;
+
+            // The game is running, so do fire off all of the processing that needs to happen
+            // every frame (updating processes, updating the physics system, etc)
+            case 'running':
                 this.get('processManager').updateProcesses(deltaTime);
+
                 /* update the physics system */
+
+                break;
+
+            default:
+                Ember.assert(false, 'Unknown state!');
+        }
 
         /* TODO: this is not a safe iteration */
         this.views.forEach(function (view) {
